29a785c7f66955a9c09338966adb16c528b72cb1
Saurabh-57 <114665845+Saurabh-57@users.noreply.github.com>
2023-02-28 18:13:41 +0530

    1 file modified

--- src/app/models/teams.ts
+++ src/app/models/teams.ts
@@ -1,15 +1,15 @@
 export interface Teams {
-    data: ITeams[];
-    id: number;
+  data: Array<ITeams>;
+  id: number;
 }
 
-interface ITeams {
-    data: Array<ITeams>;
-    abbreviation: string;
-    city: string;
-    conference: string;
-    division: string;
-    full_name: string;
-    id: number;
-    name: string;
+export interface ITeams {
+  data: Array<ITeams>;
+  abbreviation: string;
+  city: string;
+  conference: string;
+  division: string;
+  full_name: string;
+  id: number;
+  name: string;
 }
